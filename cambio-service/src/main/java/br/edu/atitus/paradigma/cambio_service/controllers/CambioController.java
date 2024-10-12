eff36c8131ce836367a0df41a5255dce4800f7ae
Raul Werner <qrananico25@gmail.com>
2024-10-11 21:33:54 -0300

    parte 1 ceser

--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
@@ -1,49 +1,57 @@
 package br.edu.atitus.paradigma.cambio_service.controllers;
 
+import br.edu.atitus.paradigma.cambio_service.clients.CotacaoClient;
+import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
+import br.edu.atitus.paradigma.cambio_service.repositories.CambioRepository;
+import br.edu.atitus.paradigma.cambio_service.response.CotacaoResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
-import br.edu.atitus.paradigma.cambio_service.repositories.CambioRepository;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
-	
-	private final CambioRepository cambioRepository;
 
-	public CambioController(CambioRepository cambioRepository) {
+	private final CambioRepository cambioRepository;
+	private final CotacaoClient cotacaoClient;
+
+	public CambioController(CambioRepository cambioRepository, CotacaoClient cotacaoClient) {
 		super();
 		this.cambioRepository = cambioRepository;
+		this.cotacaoClient = cotacaoClient;
 	}
-	
+
 	@Value("${server.port}")
 	private int porta;
-	
+
 	@GetMapping("/{valor}/{origem}/{destino}")
 	public ResponseEntity<CambioEntity> getCambio(
 			@PathVariable double valor,
 			@PathVariable String origem,
 			@PathVariable String destino) throws Exception {
-		
+
 		CambioEntity cambio = cambioRepository.findByOrigemAndDestino(origem, destino)
 				.orElseThrow(() -> new Exception("Câmbio não encontrado para esta origem e destino"));
-		
+
+		String dataCotacao = LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+
+		CotacaoResponse cotacaoResponse = cotacaoClient.getCotacaoMoedaDia(origem, dataCotacao);
+		if (cotacaoResponse != null && !cotacaoResponse.getValue().isEmpty()) {
+			double cotacaoVenda = cotacaoResponse.getValue().get(0).getCotacaoVenda();
+		}
+
 		cambio.setValorConvertido(valor * cambio.getFator());
 		cambio.setAmbiente("Cambio-Service run in port: " + porta);
+
 		return ResponseEntity.ok(cambio);
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> handleException(Exception e) {
 		String cleanMessage = e.getMessage().replaceAll("[\\r\\n]", " ");
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(cleanMessage);
 	}
-
 }
