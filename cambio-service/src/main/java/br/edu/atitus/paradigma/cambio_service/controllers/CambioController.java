579888ce255b78a37b20119ed487056ff08e39d5
Raul Werner <qrananico25@gmail.com>
2024-10-11 21:10:24 -0300

    Aula de hoje antes do desafio

--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
@@ -1,46 +1,49 @@
 package br.edu.atitus.paradigma.cambio_service.controllers;
 
-import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
-import br.edu.atitus.paradigma.cambio_service.repositories.CambioRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
+import br.edu.atitus.paradigma.cambio_service.repositories.CambioRepository;
 
 @RestController
-@RequestMapping("/cambio-service")
+@RequestMapping("cambio-service")
 public class CambioController {
+	
+	private final CambioRepository cambioRepository;
 
-    private final CambioRepository cambioRepository;
+	public CambioController(CambioRepository cambioRepository) {
+		super();
+		this.cambioRepository = cambioRepository;
+	}
+	
+	@Value("${server.port}")
+	private int porta;
+	
+	@GetMapping("/{valor}/{origem}/{destino}")
+	public ResponseEntity<CambioEntity> getCambio(
+			@PathVariable double valor,
+			@PathVariable String origem,
+			@PathVariable String destino) throws Exception {
+		
+		CambioEntity cambio = cambioRepository.findByOrigemAndDestino(origem, destino)
+				.orElseThrow(() -> new Exception("Câmbio não encontrado para esta origem e destino"));
+		
+		cambio.setValorConvertido(valor * cambio.getFator());
+		cambio.setAmbiente("Cambio-Service run in port: " + porta);
+		return ResponseEntity.ok(cambio);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		String cleanMessage = e.getMessage().replaceAll("[\\r\\n]", " ");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(cleanMessage);
+	}
 
-    @Value("${spring.application.name}")
-    private String ambiente;
-
-    public CambioController(CambioRepository cambioRepository) {
-        this.cambioRepository = cambioRepository;
-    }
-
-    @GetMapping("/{valor}/{origem}/{destino}")
-    public CambioEntity getCambio(@PathVariable("valor") double valor,
-                                  @PathVariable("origem") String origem,
-                                  @PathVariable("destino") String destino) {
-
-        Optional<CambioEntity> cambioOptional = cambioRepository.findByOrigemAndDestino(origem, destino);
-
-        if (!cambioOptional.isPresent()) {
-            throw new RuntimeException("Não foi possível encontrar a taxa de câmbio para " + origem + " para " + destino);
-        }
-
-        CambioEntity cambio = cambioOptional.get();
-
-
-        double valorConvertido = valor * cambio.getFator();
-        cambio.setValorConvertido(valorConvertido);
-        cambio.setAmbiente(ambiente); // Setar o ambiente a partir das propriedades
-
-        return cambio;
-    }
 }
--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/repositories/CambioRepository.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/repositories/CambioRepository.java
@@ -1,14 +1,15 @@
 package br.edu.atitus.paradigma.cambio_service.repositories;
 
-import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
 
 @Repository
-public interface CambioRepository extends JpaRepository<CambioEntity, Integer> {
+public interface CambioRepository extends JpaRepository<CambioEntity, Integer>{
+	
+	Optional<CambioEntity> findByOrigemAndDestino(String origem, String destino);
 
-    // Método para encontrar uma entidade Cambio pela origem e destino
-    Optional<CambioEntity> findByOrigemAndDestino(String origem, String destino);
 }
